b3908c00c86ee0231d1b63cbbc067a53cecabf77
viotalJiplk <viotal.varecha@outlook.com>
2026-08-11 18:37:51 +0200

    feat(render): add comment support

--- src/js/render/chordpro/directives.js
+++ src/js/render/chordpro/directives.js
@@ -290,8 +290,44 @@ export class Capo extends Directive {
     }
 }
 
+/**
+ * Class implementing comment link node
+ */
+export class Comment extends Directive {
+    /**@type {string} */
+    #text = "";
+    static directiveName = "comment";
+    static directiveShortcut = "c";
+    /**
+     * @param {number} line line in source 
+     * @param {number} charNumber charnumber in line in source
+     * @param {string} unnamedArgument 
+     * @param {Array} namedArguments
+     */
+    constructor(line, charNumber, unnamedArgument, namedArguments) {
+        super(line, charNumber, unnamedArgument, namedArguments);
+        if (typeof unnamedArgument !== "string") {
+            throw new SemanticsError("Comment must have string argument", line, charNumber);
+        }
+        this.#text = unnamedArgument;
+    }
+    get html() {
+        const element = document.createElement("div");
+        element.setAttribute("class", "comment");
+        element.innerText = "Komentář: " + String(this.#text);
+        return element;
+    }
+    get chordpro() {
+        let text = "";
+        if (this.#text !== "") {
+            text = "\n\n{" + this.constructor.directiveName + ": " + this.#text + "}";
+        }
+        return text;
+    }
+}
+
 /** list of known directives */
-const directiveList = [Title, Artist, Soc, Sov, Chorus, Capo];
+const directiveList = [Title, Artist, Soc, Sov, Chorus, Capo, Comment];
 
 /**
  * Search and returns directive class
